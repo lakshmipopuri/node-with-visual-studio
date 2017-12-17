8430bede56ec444ad61d119c38edf0eb5f0511d4
lakshmipopuri <lakshmi487popuri@gmail.com>
2017-12-17 22:10:16 +0530

    my project with visual studio

---
+++ insertvalues5.js
@@ -0,0 +1,25 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "sa",
+  password: "abc",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "INSERT INTO customers (name, address) VALUES ?";
+  var values = [
+    ['John', 'Highway 71'],
+    ['Peter', 'Lowstreet 4'],
+    ['Amy', 'Apple st 652'],
+    ['rama', 'Sideway 1633']
+  ];
+  con.query(sql, [values], function (err, result) {
+    if (err) throw err;
+    console.log("Number of records inserted: " + result.affectedRows);
+  });
+});
+
---
+++ sqltable.js
@@ -0,0 +1,18 @@
+var mysql = require('mssql');
+
+var con = mssql.createConnection({
+  host: "localhost",
+  user: "sa",
+  password: "abc",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+});
